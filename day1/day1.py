10217c606051860adab7198d4bb12f95157195b7
maesierra <mariaeugenia.sierra@gmail.com>
2023-12-01 19:59:44 +0000

    day 1
    Much trickier for the overlapping on the second part
    Not much complain about pyhton... regexp support is nice-ish
    comprehensions are hard and things like ",".join are weird

--- day1/day1.py
+++ day1/day1.py
@@ -1,4 +1,5 @@
 import os
+import re
 import sys
 
 
@@ -11,15 +12,49 @@ class Day1Solution(Solution):
     def __init__(self) -> None:
         super().__init__(1)
 
+    def _extract_number(self, line): 
+        r = re.findall(r"[0-9]", line)
+        return int(r[0] + r[-1])
+    
     def solve_part_1(self, input, args):
         lines = self.input_to_lines(input)
-        print("Hello")
-        print(lines)
-        return 0
+        numbers = [self._extract_number(l) for l in lines]
+        return sum(numbers)
+    
+    def _extract_number_with_text_digits(self, line): 
+        digits_map = {
+            "one" : "1",
+            "two" : "2",
+            "three" : "3",
+            "four" : "4",
+            "five" : "5",
+            "six" : "6",
+            "seven" : "7",
+            "eight" : "8",
+            "nine" : "9",
+            "1": "1",
+            "2": "2",
+            "3": "3",
+            "4": "4",
+            "5": "5",
+            "6": "6",
+            "7": "7",
+            "8": "8",
+            "9": "9",
+            "0": "0"
+        }
+        digits = []
+        for pos in range(0, len(line)): 
+            match = re.match(r"[0-9]|one|two|three|four|five|six|seven|eight|nine", line[pos:])
+            if match: 
+                digits.append(digits_map[match.group()])
+        return int(digits[0]+ digits[-1])
+    
     
     def solve_part_2(self, input, args):
-        print(input)
-        return 2
+        lines = self.input_to_lines(input)
+        numbers = [self._extract_number_with_text_digits(l) for l in lines]
+        return sum(numbers)
 
 if __name__ == '__main__':    
     day1 = Day1Solution()
--- day1/day1_test.py
+++ day1/day1_test.py
@@ -13,28 +13,27 @@ class Day1SolutionTest(unittest.TestCase):
 
 
     def test_part1(self):
-        lines = """a
-b
-c
-d
-e
-""" 
+        lines = """1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet""" 
         with text_to_input(lines) as input: 
             solver = Day1Solution()
             solution = solver.solve_part_1(input, [])
-            self.assertEqual(solution, 0)
+            self.assertEqual(solution, 142)
 
     def test_part2(self):
-        lines = """a
-b
-c
-d
-e
-""" 
+        lines = """two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen""" 
         with text_to_input(lines) as input: 
             solver = Day1Solution()
             solution = solver.solve_part_2(input, [])
-            self.assertEqual(solution, 2)
+            self.assertEqual(solution, 281)
 
 
 if __name__ == '__main__':
