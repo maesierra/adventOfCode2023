31f3b0502f477c0bb1b281c980f8683df7d5dee0
maesierra <mariaeugenia.sierra@gmail.com>
2023-12-07 02:49:48 +0000

    day 5: complete
    what a pain... Just when I was going to go to bed the inverse solution yield the result.
    A really shitty solution. The brute force was supposed to take 6 hours or so...
    The only clever solution I could think of was to do and inverse traslation from location to soil
    So I can go up until a find a seed in the ranges
    I gave up on that solution yesterday, but in the end only needed about an hour or so
    Happy this problem is over.

--- day5/day5.py
+++ day5/day5.py
@@ -1,7 +1,8 @@
 import os
 import sys
 import re
-import numpy as np
+import time
+import more_itertools as mit
 
 
 root_folder = os.path.abspath(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
@@ -18,25 +19,46 @@ class Range():
     def contains(self, value:int) -> int:
         return value >= self.src_start and value < self.src_start + self.len
     
-    def translate(self, value:int) -> int:
+    def to_dest(self, value:int) -> int:
         return (value - self.src_start) + self.dest_start
+    
+    def contained_in_dest(self, value:int) -> int:
+        return value >= self.dest_start and value < self.dest_start + self.len
+    
+    def from_dest(self, value:int) -> int:
+        return (value - self.dest_start) + self.src_start
 
 class SeedMap():
     def __init__(self, src:str, dest:str) -> None:
         self.src = src
         self.dest = dest
         self.ranges = []
+        self.min = None
+        self.max = None
         
 
     def add_range(self, src_start, dest_start, len):
         self.ranges.append(Range(src_start=src_start, dest_start=dest_start, len=len))
+        self.ranges = sorted(self.ranges, key=lambda r: r.src_start)
+        self.min = self.ranges[0].src_start
+        self.max = self.ranges[-1].src_start + len
 
-    def translate(self, value:int) -> int:
+    def to_dest(self, value:int) -> int:
+        if value < self.min or value > self.max:
+            return value
         for r in self.ranges:
             if r.contains(value):
-                return r.translate(value)
+                return r.to_dest(value)
         return value    
-        
+    
+    def from_dest(self, value:int) -> int:
+        if len([r for r in self.ranges if r.contained_in_dest(value)]) > 1:
+            raise Exception(f"{value} Found in multiple ranges in {self.src}")
+        for r in self.ranges:
+            if r.contained_in_dest(value):
+                return r.from_dest(value)
+        return value    
+    
 class Day5Solution(Solution): 
     def __init__(self) -> None:
         super().__init__(5)
@@ -52,29 +74,79 @@ class Day5Solution(Solution):
             maps[map.src] = map
         return maps
     
-    def _translate(self, value:int, src:str, dest:str, maps:dict) -> int: 
+    def _translate_to_dest(self, value:int, src:str, dest:str, maps:dict) -> int: 
         current_map = src
         current_value = value
         while current_map != dest:
             map = maps[current_map]
-            new_value = map.translate(current_value)
-            print(f"Translated from {current_map} {current_value} to {map.dest} {new_value}")
+            new_value = map.to_dest(current_value)
+            # print(f"Translated from {current_map} {current_value} to {map.dest} {new_value}")
             current_value = new_value
             current_map = map.dest
+        return current_value
+    
+    def _translate_from_dest(self, value:int, src:str, dest:str, maps:dict) -> int: 
+        map = mit.nth([m for m in maps.values() if m.dest == dest], 0)
+        current_value = value
+        while map:
+            new_value = map.from_dest(current_value)
+            current_value = new_value
+            map = mit.nth([m for m in maps.values() if m.dest == map.src], 0)
         return current_value
 
     def solve_part_1(self, input, args):
         lines = self.input_to_lines(input)
         seeds = [int(s) for s in re.split(r"[ :]", lines[0]) if s.isnumeric()]
         maps = self._parse_maps(lines=lines)
-        locations = [self._translate(value=s, src="seed", dest="location", maps=maps) for s in seeds]
+        locations = [self._translate_to_dest(value=s, src="seed", dest="location", maps=maps) for s in seeds]
         return min(locations)
     
     def solve_part_2(self, input, args):
         lines = self.input_to_lines(input)
-        seed_ranges = np.array_split([int(s) for s in re.split(r"[ :]", lines[0]) if s.isnumeric()], 2)
-        maps = self._parse_maps(lines=lines)
-        return 2
+        seed_ranges = sorted([range(start, start + len) for start, len in list(mit.chunked([int(s) for s in re.split(r"[ :]", lines[0]) if s.isnumeric()], 2))], key=lambda r: r.start)
+        maps = self._parse_maps(lines=lines)        
+        # return self.brute_force_direct(maps, seed_ranges)
+        return self.brute_force_inverse(maps, seed_ranges)
+
+    def brute_force_inverse(self, maps, seed_ranges):
+        value  = 0
+        start = time.process_time()
+        while True:                        
+            seed = self._translate_from_dest(value=value, src="seed", dest="location", maps=maps)            
+            if value and value % 10000 == 0:                     
+                    print(f"Checking location {value} <= seed {seed}. Elapsed {time.process_time() - start}")                    
+                    start = time.process_time()
+            # translated = self._translate_to_dest(value=seed, src="seed", dest="location", maps=maps)
+            # if translated != value:
+            #     raise Exception(f"From {value} to {seed} does not match {seed} to {translated}")
+            # print(f"Checking seed: {seed}/{self._translate_to_dest(value=seed, src="seed", dest="location", maps=maps)} => location:{value}")
+            for r in seed_ranges:
+                if seed in r:
+                    return value                            
+            value += 1
+    def brute_force_direct(self, maps, seed_ranges):
+        n_ranges = len(seed_ranges)
+        total = sum([r.stop - r.start for r in seed_ranges])       
+        skip = 0 
+        min = 0
+        current = 0
+        start = time.process_time()                    
+        for current_range, r in enumerate(seed_ranges):      
+            if current < skip and r.stop < skip:
+                current += r.stop - r.start
+                continue                  
+            for seed in r:                 
+                if current < skip:
+                    current += 1
+                    continue                               
+                if current and current % 1000000 == 0:                     
+                    print(f"Checking {current}/{total} [range {current_range}/{n_ranges}]. Elapsed {time.process_time() - start} Min: {min}")                    
+                    start = time.process_time()
+                location = self._translate_to_dest(value=seed, src="seed", dest="location", maps=maps)
+                if not min or location < min:
+                    min = location
+                current += 1
+        return min
 
 if __name__ == '__main__':    
     day5 = Day5Solution()
