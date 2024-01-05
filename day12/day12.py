e35f6c4116dce128ee8252a69952dd9264bf9131
Maria-Eugenia Sierra <mariaeugeniap@spotify.com>
2024-01-05 15:23:36 +0100

    day 12. Part 2
    After days trying to figure out new algorithms, that in theory it will be non-recursive and cacheable... They did not work.
    
    Looking for hints, it seems that my idea to try all the possible options was correct but with a couple of tweaks.
    
    No more trying to match as much as possible, one group match until the end is reached. At the end if all the groups are consumed, it's a single match
    The cache key it's the group index, not the matched groups. That means that the caches prunes a lot of the options already calculated.

--- day12/day12.py
+++ day12/day12.py
@@ -1,130 +1,215 @@
 import os
 import re
 import sys
-from collections import Counter
+from collections import Counter, deque
+from typing import Deque, Dict, List, Tuple
 
 root_folder = os.path.abspath(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 sys.path.append(root_folder)
 
 from advent_of_code import Solution
 
-class Cache(): 
-    def __init__(self) -> None:
-        self.dict = {}
-    
-    def _key(self, fragment:str, groups:list):
-        return f"{fragment} {",".join([str(g) for g in groups])}"
-    
-    def put(self, fragment:str, groups:list, value:int):
-        self.dict[self._key(fragment=fragment, groups=groups)] = value
-
-    def get(self, fragment:str, groups:list): 
-        return self.dict.get(self._key(fragment=fragment, groups=groups), None)
-
         
 
 class Day12Solution(Solution): 
     def __init__(self) -> None:
         super().__init__(12)
-        self.cache = Cache()
+        self.cache:Dict[str, List[List[int]]] = {}
 
-    def calculate_arrangements(self, groups, line, pos = 0, matched="") -> int:
-        unmatched_at_start = line[pos:]
-        while groups:            
-            unmatched = line[pos:]
-            cached = self.cache.get(fragment=unmatched, groups=groups)
-            # if cached is not None:
-            #     if self.debug:
-            #         print(f"Cache hit with {unmatched} {groups}")
-            #     return cached
-            #Basic consistency checks
-            c = Counter(unmatched)
-            if c.get('?', 0) + c.get('#', 0) < sum(groups):
-                # Not enough ? to be converted into #
-                print(f"{line} => INVALID (case1) matched={matched} unmatched={line[pos:]} groups={groups}")
-                self.cache.put(fragment=unmatched_at_start, groups=groups, value=0)
-                return 0
-            # Remove all . from the borders                        
-            match = re.search(r"^\.+|\?+|#+", unmatched)
-            token = match.group()
-            if token[0] == '.':
-                # Just match the separators
-                pos +=  len(token)
-                matched += token
-                if self.debug:
-                    print(f"{line} => matched={matched} unmatched={line[pos:]} groups={groups}")
-            elif token[0] == '#':
-                size = groups[0]
-                # Invalid option
-                if len(unmatched) < size or size < len(token):
-                    if self.debug:
-                        print(f"{line} => INVALID (case2) matched={matched} unmatched={line[pos:]} groups={groups}")
-                    self.cache.put(fragment=unmatched_at_start, groups=groups, value=0)
-                    return 0
+    def calculate_arrangements(self, groups, line, pos = 0, current_group = 0) -> int:
+        key = f"{pos}-{current_group}"
+        cached = self.cache.get(key, None)
+        if cached is not None:
+            return cached
+        
+        #If we reach the end, everything must be consumed
+        if pos >= len(line):
+            return 1 if current_group == len(groups) else 0
+        if current_group > len(groups):
+            return 0 #No more groups to consume
+
+        unmatched = line[pos:]        
+        c = Counter(unmatched)
+        n_unknown = c.get('?', 0)
+        if n_unknown + c.get('#', 0) < sum(groups[current_group:]):
+            # Not enough ? to be converted into #                
+            return 0
+        if n_unknown == len(unmatched) and current_group == len(groups):
+            return 1
+        
+        # We try to capture groups or . or # but for ? we only take one
+        match = re.search(r"^\.+|\?|#+", unmatched)
+        token = match.group()
+        n_arrangements = 0
+        if token[0] != '#': # ? or .
+            # Continue
+            n_arrangements += self.calculate_arrangements(groups, line, pos + len(token), current_group)
+        if token[0] != '.': # ? or #
+            invalid_option = current_group >= len(groups)
+            if not invalid_option:
+                group = groups[current_group]
+                # Invalid option 
+                invalid_option = token[0] == '#' and len(token) > group
+            if not invalid_option: 
                 # We always consume up to group size, regardless if is unknown after
-                token = unmatched[:size]
-                if '.' in token:
-                    # Invalid option
-                    if self.debug:
-                        print(f"{line} => INVALID (case3) matched={matched} unmatched={line[pos:]} groups={groups}")
-                    self.cache.put(fragment=unmatched_at_start, groups=groups, value=0)
-                    return 0   
-                pos += size
-                matched += '#'*size
-                groups = groups[1:]
-                if not groups:
-                    # Need to check if no more #
-                    if '#' in unmatched[size:]:
-                        if self.debug:
-                            print(f"{line} => INVALID (case 4) matched={matched} unmatched={line[pos:]} groups={groups}")
-                        self.cache.put(fragment=unmatched_at_start, groups=groups, value=0)
-                        return 0
-                    if self.debug:                    
-                        print(f"{line} => MATCH FOUND(case 1) matched={matched}")
-                    self.cache.put(fragment=unmatched_at_start, groups=groups, value=1)
-                    return 1                                                
-                if unmatched[size] == '#':
-                    if self.debug:
-                        print(f"{line} => INVALID (case 5) matched={matched} unmatched={line[pos:]} groups={groups}")
-                    self.cache.put(fragment=unmatched_at_start, groups=groups, value=0)
-                    return 0   
+                token = unmatched[:group]
+                invalid_option = '.' in token
+            if not invalid_option:
+                # pos + group + 1 => To take into a account the . after a #
+                if pos + group != len(line): #not a exact match                                    
+                    invalid_option = line[min(len(line) - 1, pos + group)] == '#'
+                pos += group + 1
+            if not invalid_option: 
+                n_arrangements += self.calculate_arrangements(groups, line, pos, current_group + 1)
+        
+        self.cache[key] = n_arrangements
+        return n_arrangements
 
-            # elif len(token) == sum(groups) + len(groups) - 1:
-            #      if self.debug:
-            #             print(f"{line} => MATCH FOUND (case2) matched={matched + '.'.join(['#'*g for g in groups])}")
-            #      return 1
-            # elif len(token) == len(unmatched):
-            #     if self.debug:                    
-            #         print(f"{line} => MATCH FOUND (case 3) matched={matched} unmatched={line[pos:]} groups={groups}")                    
-            #     return sum([n for n in range(0, len(token) - sum(groups))])
-            else:                
-                n_arrangements = self.calculate_arrangements(line=matched + '.' + unmatched[1:], pos=pos, groups=groups, matched=matched)
-                if not matched or matched[-1] != '#':
-                    n_arrangements += self.calculate_arrangements(line=matched + '#' + unmatched[1:], pos=pos, groups=groups, matched=matched)                
-                if self.debug:                    
-                    print(f"{line} completed with {n_arrangements} found.")
-                self.cache.put(fragment=unmatched_at_start, groups=groups, value=n_arrangements)
-                return n_arrangements
+
+    # def _match(self, text:str, group:int):
+    #     if len(text) == group:
+    #         return True
+    #     return False
+    
+    # def _options(self, text:str, groups:list[int]):
+    #     if len(groups) == 1:
+    #         return len(text) - groups[0] + 1
+    #     elif len(groups) == 0:
+    #         return 0        
+    #     groups = groups.copy()
+    #     required = sum(groups) + (len(groups) - 1)
+    #     diff =  len(text) - required
+    #     if diff == 0:
+    #         return 1
+    #     g = groups.pop(0)
+    #     n_options = 0 
+    #     for i in range(0, diff + 1):
+    #         n_options += self._options(text[g + 1 + i:], groups)
+    #     return n_options
+
+
+         
+
+    
+    # def _matches(self, text:str, groups:List[int], full=False) -> List[int]:
+    #     matched = [len(s) for s in text.split('.') if s]
+    #     if matched and matched == (groups if full else groups[:len(matched)]):
+    #         return matched
+    #     elif len(matched) > 1 and not full:
+    #         #See if there is a partial match
+    #         matched = matched[:-1]
+    #         while matched: 
+    #             if matched == groups[:len(matched)]:
+    #                 return matched
+    #             matched = matched[:-1]
+    #         return []
+    #     else:
+    #         return []
+        
+    # def _increase_option(self, ancestors, option:str, groups:List[int], n):
+    #     for fragment, group in reversed(ancestors):
+    #         groups = [group] + groups
+    #         option = fragment + option
+    #         key = f"{option} {','.join([str(d) for d in groups])}"
+    #         self.cache[key] = self.cache.get(key, 0) + n
+    #     return n
             
 
-        return n_arrangements
+
+    # def process_line(self, line:str, groups:List[int]) -> int:
+    #     n_options = 0
+    #     fragments:Deque[Tuple(str, List[int])] = deque()
+    #     fragments.appendleft((str(line), groups.copy(), []))
+    #     while fragments: 
+    #         unmatched, groups, ancestors = fragments.popleft()
+    #         cached = self.cache.get(unmatched, None)
+    #         if cached:
+    #             print(f"using cached result for {unmatched} {cached}")
+    #             n_options += cached
+    #             continue
+    #         current = str(unmatched)            
+    #         #Remove all operational from borders
+    #         unmatched = unmatched.strip('.')             
+    #         current_offset = len(current) - len(unmatched)
+    #         c = Counter(unmatched)
+    #         n_unknown = c.get('?', 0)
+    #         if n_unknown == len(unmatched):
+    #             n_options += self._increase_option(ancestors, current, groups, self._options(unmatched, groups))
+    #             continue
+    #         elif n_unknown == 0:
+    #             if self._matches(unmatched, groups, full=True):
+    #                 n_options += self._increase_option(ancestors, current, groups, 1)
+    #             continue
+    #         elif len(groups) == 1 and groups[0] == len(unmatched):
+    #             if c.get('.', 0) == 0:
+    #                 n_options += self._increase_option(ancestors, current, groups, 1)
+    #             continue
+    #         #try to determine how many options group 1 has 
+    #         # We need to match at least up to group[0], at most len - (sum(remaining_groups) + len(remaining_groups))
+    #         group = groups[0]
+    #         remaining_groups = groups[1:]
+    #         min_remaining_space = sum(remaining_groups) + len(remaining_groups)
+    #         # Edge case in which min_remaining_space is fixed to #. Move it to the first ? or . before it
+    #         while min_remaining_space < len(unmatched) and unmatched[-min_remaining_space] == '#':
+    #             min_remaining_space += 1            
+    #         diff = len(unmatched) - min_remaining_space - group + 1    
+    #         if diff < 0:
+    #             continue            
+    #         fragment = unmatched[group + diff:]            
+    #         options = []
+    #         used_options = set()
+    #         fragments_added = set()
+    #         for i in range(0, diff):
+    #             # We're going to try all the options to put group in the available space. 
+    #             # Eg for ????#???#.?????#?? group=7 1,3
+    #             # we'll try the options bellow and we'll discard the options that don't match the pattern
+    #             # #######......??#??
+    #             # .#######.....??#??
+    #             # ..#######....??#??
+    #             # ...#######...??#??
+    #             # ....#######..??#??
+    #             # .....#######.??#??
+    #             option:str = unmatched[:i].replace('?', '.') + unmatched[i:group + i].replace('?', '#') + unmatched[group + i:group + diff].replace('?', '.')
+    #             if option not in used_options: 
+    #                 used_options.add(option)
+    #                 matched = self._matches(option, groups, full=len(groups) == 1)
+    #                 if matched:
+    #                     if fragment: 
+    #                         # Next fragment will start after the last # in the first matched group plus an extra .
+    #                         pos = 0
+    #                         while option[pos] == '.':
+    #                             pos += 1                            
+    #                         pos += matched[0] + 1                                
+    #                         next_fragment = unmatched[pos:]
+    #                         next_groups = groups[1:]
+    #                         if option[:pos] not in fragments_added and next_groups: 
+    #                             fragments.appendleft((next_fragment, next_groups, ancestors + [(current[:pos + current_offset], groups[0])]))
+    #                             options.append(matched)
+    #                             fragments_added.add(option[:pos])
+    #                     else: 
+    #                         n_options += self._increase_option(ancestors, current, groups, 1)
+                
+
+    #     return max(1, n_options)
     
-        
-
-
 
     def solve_part_1(self, input, args):
         lines = self.input_to_lines(input)
-        sum = 0
+        res = 0
         for i, line in enumerate(lines):
             line, groups = line.split(" ")
-            groups = [int(d) for d in groups.split(",")]                                
-            res = self.calculate_arrangements(line=line, groups=groups)
-            sum += res
-            print(f"Line {i}/{len(lines)} {line} => {res}")            
-        return sum
+            groups = [int(d) for d in groups.split(",")]
+            self.cache.clear()
+            n_options = self.calculate_arrangements(line=line, groups=groups)
+            if self.debug:
+                print(f"{line} => {n_options}")
+            res += n_options
+                        
+        return res
     
     def solve_part_2(self, input, args):
+        # Solve part1 agin to fill the cache
+        self.solve_part_1(input, [])
         lines = self.input_to_lines(input)
         sum = 0
         for i, line in enumerate(lines):
@@ -132,8 +217,9 @@ class Day12Solution(Solution):
             folded_groups = [int(d) for d in folded_groups.split(",")]
             line = "?".join([folded_line for i in range(0, 5)])
             groups = []
-            for i in range(0, 5):
+            for _ in range(0, 5):
                 groups.extend(folded_groups)
+            self.cache.clear()
             res = self.calculate_arrangements(line=line, groups=groups)
             sum += res
             print(f"Line {i}/{len(lines)} {line} => {res}")            
@@ -142,5 +228,3 @@ class Day12Solution(Solution):
 if __name__ == '__main__':    
     day12 = Day12Solution()
     day12.run()
-
-# 000 ???#.?#????#?.?..?? 3,1,4,1,2 => 0    
--- day12/day12_test.py
+++ day12/day12_test.py
@@ -11,41 +11,81 @@ from test_utils import text_to_input
 
 class Day12SolutionTest(unittest.TestCase):
 
-    def test_caculate_arragements(self):
-        solver = Day12Solution()
-        solver.debug = True
-        test_cases = [
-            # ['???.### 1,1,3', 1],
-            # ['.??..??...?##. 1,1,3', 4],
-            # ['?#?#?#?#?#?#?#? 1,3,1,6', 1],
-            # ['????.#...#... 4,1,1', 1],
-            # ['????.######..#####. 1,6,5', 4],
-            # ['?###???????? 3,2,1', 10],
-            # ['???#.?#????#?.?..?? 3,1,4,1,2', 2],
-            ['????#???#.?????#?? 7,1,3', 12]
-        ]
-        for line, expected in test_cases:
-            line, groups = line.split(" ")
-            groups = [int(d) for d in groups.split(",")]
-            self.assertEqual(solver.calculate_arrangements(line=line, groups=groups), expected)
+    # def test_options(self):
+    #     solver = Day12Solution()
+    #     solver.debug = True
+    #     test_cases = [
+    #         ('???', [1,1], 1),
+    #         ('???????', [2, 1], 10),
+    #         ('??????', [1,1], 10,),
+    #         ('??????????', [2,2], 21,),
+    #         ('??????????????????',[1,3,9], 20),
+    #         ('??????????????????',[1,3,2,5], 70),
+    #     ]
+    #     for text, groups, expected in test_cases:            
+    #         options = solver._options(text, groups)
+    #         self.assertEqual(options, expected)
+
+    # def test_process_line(self):
+    #     solver = Day12Solution()
+    #     solver.debug = True
+    #     test_cases = [
+    #         ('???.????.#?.??? 2,2', 7),
+    #         ('?###.?#???????#??? 4,2,1,3', 21),
+    #         ('.???#???..??.#???? 6,3', 2),
+    #         ('???????#????? 1,3', 17),
+    #         ('??????.?????#??# 1,3,2,1,1', 9),
+    #         ('.??..##??#?#?#????? 2,2,2,1,1,1', 4),
+    #         ('????#??##??.????#. 2,5,5', 3),
+    #         ('???.??##..?.?????.? 1,3,3', 9),
+    #         ('????#???#.?????#?? 7,1,3', 12),
+    #         ('????.######..#####. 1,6,5', 4),
+    #         ('.??..??...?##. 1,1,3', 4),
+    #         ('???.### 1,1,3', 1),
+    #         ('?#?#?#?#?#?#?#? 1,3,1,6', 1),
+    #         ('????.#...#... 4,1,1', 1),
+    #         ('?###???????? 3,2,1', 10),
+    #         ('???#.?#????#?.?..?? 3,1,4,1,2', 2),
+    #     ]
+    #     self.maxDiff = None
+    #     res = []
+    #     for line, _ in test_cases:
+    #         l, groups = line.split(" ")
+    #         groups = [int(d) for d in groups.split(",")]
+    #         res.append((line, solver.process_line(line=l, groups=groups)))    
+    #     self.assertEqual(res, test_cases)
     
-    def test_caculate_arragements_all(self):
+
+    def test_calculate_arrangements(self):
         solver = Day12Solution()
         solver.debug = True
         test_cases = [
-            ['???.### 1,1,3', 1],
-            ['.??..??...?##. 1,1,3', 4],
-            ['?#?#?#?#?#?#?#? 1,3,1,6', 1],
-            ['????.#...#... 4,1,1', 1],
-            ['????.######..#####. 1,6,5', 4],
-            ['?###???????? 3,2,1', 10],
-            ['???#.?#????#?.?..?? 3,1,4,1,2', 2],
+            ('??????.?????#??# 1,3,2,1,1', 9),
+            ('?###.?#???????#??? 4,2,1,3', 21),
+            ('???.????.#?.??? 2,2', 7),
+            ('.???#???..??.#???? 6,3', 2),
+            ('???????#????? 1,3', 17),
+            ('.??..##??#?#?#????? 2,2,2,1,1,1', 4),
+            ('????#??##??.????#. 2,5,5', 3),
+            ('???.??##..?.?????.? 1,3,3', 9),
+            ('????#???#.?????#?? 7,1,3', 12),
+            ('????.######..#####. 1,6,5', 4),
+            ('.??..??...?##. 1,1,3', 4),
+            ('???.### 1,1,3', 1),
+            ('?#?#?#?#?#?#?#? 1,3,1,6', 1),
+            ('????.#...#... 4,1,1', 1),
+            ('?###???????? 3,2,1', 10),
+            ('???#.?#????#?.?..?? 3,1,4,1,2', 2),
         ]
-        for line, expected in test_cases:
-            line, groups = line.split(" ")
+        self.maxDiff = None
+        res = []
+        for line, _ in test_cases:
+            l, groups = line.split(" ")
             groups = [int(d) for d in groups.split(",")]
-            self.assertEqual(solver.calculate_arrangements(line=line, groups=groups), expected)
-            print(f"{line} => OK")
+            solver.cache.clear()
+            res.append((line, solver.calculate_arrangements(line=l, groups=groups)))    
+        self.assertEqual(res, test_cases)
+    
 
     def test_part1(self):
         lines = """???.### 1,1,3
@@ -59,6 +99,14 @@ class Day12SolutionTest(unittest.TestCase):
             solver.debug = True
             solution = solver.solve_part_1(input, [])
             self.assertEqual(solution, 21)
+
+    def test_part1_real_input(self):
+        input_file = os.path.join(os.path.dirname(__file__), "../", "input_12")
+        solver = Day12Solution()
+        solver.debug = False
+        solution = solver.solve_part_1(input_file, [])
+        self.assertGreater(solution, 0)
+
 
     def test_part2(self):
         lines = """???.### 1,1,3
